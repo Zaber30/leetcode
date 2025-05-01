45bf110bb38c4fb1d5a24624293ba84c78cda534
Zaber30 <alzaberalzaber23@gmail.com>
2025-05-01 21:02:05 +0600

    first commit

---
+++ 1.two-sum.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+ // @lc code=start
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i;
+        for (i = 0; i < nums.size();i++) {
+            
+        }
+    }
+};
+// @lc code=end
+
